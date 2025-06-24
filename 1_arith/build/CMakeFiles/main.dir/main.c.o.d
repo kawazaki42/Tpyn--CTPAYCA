90f6780f9ec9201ce65f7c7ff6fc3e640d01a201
kawazaki42 <>
2025-06-24 22:40:32 +0900

    buncha files from cmake

---
+++ 1_arith/build/CMakeFiles/main.dir/main.c.o.d
@@ -0,0 +1,15 @@
+CMakeFiles/main.dir/main.c.o: \
+ C:/Users/Николай/Desktop/урра\ я\ умнее\ чем\ информатичка/Tpyn--CTPAYCA/1_arith/main.c \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/math.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/crtdefs.h
