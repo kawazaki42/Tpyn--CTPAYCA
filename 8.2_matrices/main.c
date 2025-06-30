090e8bbcf443f8eda2d1b83cd0ec2b37782b3e04
anonymous <anonymous>
2025-06-30 20:52:25 +0900

    (8.2) Change UI interaction

--- 8.2_matrices/main.c
+++ 8.2_matrices/main.c
@@ -52,43 +52,58 @@ int main() {
     setlocale(LC_ALL, "en_US.UTF8");
 
 
-    /// Порядок матрицы
+    // Ввод данных
+
+    /// Порядок матрицы, размерность векторов
     int mat_order;
+    // printf("Размерность линейного пространства (n): ");
     printf("Порядок матрицы (n): ");
     scanf_s("%i", &mat_order);
 
-    printf("Для каждого вектора введите %d"
-           " действительных чисел через пробел:\n",
-           mat_order);
+    putchar('\n');
+    printf("Введите %d действительных чисел (%dx%d).\n",
+           mat_order*mat_order, mat_order, mat_order);
 
+    printf("(Разделители - пробел и перевод строки)\n");
+    putchar('\n');
+    printf("Матрица A:\n");
+    matrix_of_float A = read_matrix_of_float(mat_order, mat_order);
+
+    putchar('\n');
+    printf("Для каждого вектора введите n действительных чисел\n");
+
+    printf("(Разделитель - пробел)\n");
+    
+    printf("(n = %d)\n", mat_order);
+
+    putchar('\n');
     printf("Вектор x: ");
     array_of_float x = read_array_of_float(mat_order);
 
     printf("Вектор y: ");   
     array_of_float y = read_array_of_float(mat_order);
     
-    putchar('\n');
-    printf("Матрица A (%dx%d)\n", mat_order, mat_order);
-    printf("Введите действительне числа через пробел.\n");
-    matrix_of_float A = read_matrix_of_float(mat_order, mat_order);
 
-
+    // Вычисления
     vec_add_inplace(x, y, mat_order);
     free(y);
-
     y = multiply_mat_vec(A, x, mat_order);
 
 
-    putchar('\n');
+    // Вывод
 
+    putchar('\n');
     printf("A(x + y) = (");
 
+    // Все элементы, кроме последнего, через запятую
     for(size_t i = 0; i < mat_order-1; i++)
-        printf("%8.2f, ", y[i]);
+        printf("%.2f, ", y[i]);
 
-    printf("%8.2f)\n", y[mat_order-1]);
+    printf("%.2f)", y[mat_order-1]);  // последний
+    putchar('\n');
     
 
+    // Завершение
     free(x);
     free(y);
     delete_matrix_of_float(A, mat_order);
