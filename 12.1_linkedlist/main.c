94aae6c5bed5093ebf8af447821656bae371efc1
anonymous <anonymous>
2025-07-02 18:11:16 +0900

    (12.1) Document main.c

--- 12.1_linkedlist/main.c
+++ 12.1_linkedlist/main.c
@@ -11,9 +11,9 @@
  */
 
 #include <stdlib.h>  // EXIT_SUCCESS
-#include <stdio.h>
+#include <stdio.h>   // scanf, printf
 #include <locale.h>  // setlocale
-#include "sllist.h"
+#include "sllist.h"  // SLList_* (односвязные списки)
 
 int main() {
     // Установить кодировку UTF-8
@@ -26,8 +26,14 @@ int main() {
     scanf_s("%zu", &n);  // u - беззнаковый,
                          // z - длиной с size_t
 
-    struct SLList_node *head, *tail;
-    head = tail = NULL;  // изначально пусты
+
+    // Односвязный список из вещественных чисел
+
+    /// Голова (первый элемент)
+    struct SLList_node *head;
+    /// Хвост (последний элемент)
+    struct SLList_node *tail;
+    head = tail = NULL;  // изначально список пуст
 
     printf_s("Введите %zu вещественных чисел через пробел:\n", n);
 
@@ -36,21 +42,25 @@ int main() {
         float data;
         scanf_s("%f", &data);
 
+        // Добавление в конец (append)
         tail = SLList_new_tail(tail, data);
 
         if(i == 0)
+            // иначе голова останется NULL
             head = tail;
     }
 
     
+    /// Итератор цикла
+    struct SLList_node *cur;
     float sum = 0.0;
     for(
-        struct SLList_node *cur = head;
+        cur = head;
         cur != NULL;
         cur = cur->next
     ) {
         float x = cur->data;
-        sum += x*x;
+        sum += x*x;  // x^2
     }
 
     printf_s("Сумма квадратов посл-ти: %.2f\n", sum);
