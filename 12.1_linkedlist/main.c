3fb6f87ac699d47a4ade4433d44067356f6b81e8
student <>
2025-07-02 12:09:51 +0900

    (12.1) template

---
+++ 12.1_linkedlist/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @author  Николай Ковалев
+ * 
+ * @details (136) Даны натуральное число n, действительные числа a1,..., an.
+ *                Вычислить:
+ *             Д) a1^2 + ... + an^2;
+ * 
+ * Ссылка:
+ *     https://ivtipm.github.io/Programming/Glava06/index06.htm#z136
+ */
+
+#include "sllist.h"
+
+int main() {
+
+}
---
+++ lib/sllist.c
@@ -0,0 +1,34 @@
+#include <stddef.h>  // NULL
+#include "sllist.h"
+
+struct SLList_node *SLList_new_head(struct SLList_node *old_head, float data) {
+    struct SLList_node *result = malloc(sizeof *result);
+    result->data = data;
+    result->next = NULL;
+    return result;
+}
+
+
+struct SLList_node *SLList_new_tail(struct SLList_node *old_tail, float data) {
+    struct SLList_node *result = malloc(sizeof *result);
+    result->data = data;
+    result->next = NULL;
+    old_tail->next = result;
+    return result;
+}
+
+
+void SLList_delete(struct SLList_node *head) {
+    if(!head) return;
+
+    struct SLList_node *cur, *next;
+
+    cur = head;
+    next = head->next;
+    
+    while(head != NULL) {
+        free(cur);
+        cur = next;
+        next = cur->next;
+    }
+}
