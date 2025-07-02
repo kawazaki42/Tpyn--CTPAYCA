0ea52993ef183c0aa8a9913cc6edab0e579228cc
anonymous <anonymous>
2025-07-02 20:15:21 +0900

    pack some minor changes

--- 12.1_linkedlist/main.c
+++ 12.1_linkedlist/main.c
@@ -5,8 +5,7 @@
  * @details (136) Даны натуральное число n, действительные числа a1,..., an.
  *                Вычислить:
  *             Д) a1^2 + ... + an^2;
- * 
- * Ссылка:
+ * @see
  *     https://ivtipm.github.io/Programming/Glava06/index06.htm#z136
  */
 
