d6b8a5ea2c69e49e0ddf54c87f72868fb71d0509
anonymous <anonymous>
2025-07-02 21:32:11 +0900

    (12.1) document sllist.c

--- mylibs/sllist.c
+++ mylibs/sllist.c
@@ -10,23 +10,23 @@
 
 /**
  * @brief Добавить элемент в начало списка (перед головой).
- *        Пример:
  * @param old_head текущая голова списка
+ *                 (`NULL`, если требуется создать список)
  * @param data вещественное число (`float`) для добавления
  * @return Новую голову списка
  * 
- * Следует сохранить новую голову списка, например:
- * 
- * ```c
- *     head = SLList_new_head(head, 3.14);
- * ```
- * 
- *     Иначе она будет потеряна.
+ * @note
+ *   Следует сохранить новую голову списка, например:
+ *   ```c
+ *       head = SLList_new_head(head, 3.14);
+ *   ```
+ *   Иначе она будет потеряна.
  */
 struct SLList_node *SLList_new_head(
     struct SLList_node *old_head,
     float data
 ) {
+    // Создание узла
     struct SLList_node *result = malloc(sizeof *result);
     result->data = data;
     result->next = old_head;  // Возможно, NULL
@@ -37,42 +37,57 @@ struct SLList_node *SLList_new_head(
 
 /**
  * @brief Добавить элемент в конец списка (после хвоста).
- *   ```c
- *       tail = SLList_new_tail(tail, 3.14);
- *  ```
  * @param old_tail текущий хвост списка
+ *                 (`NULL`, если требуется создать список)
  * @param data вещественное число (`float`) для добавления
  * @return Новый хвост списка
  * @note
- *     Следует сохранить новый хвост списка, например:
- * 
- *     Иначе придется искать 
+ *   Для дальнейшей работы с концом списка рекомендуется
+ *   сохранить новый хвост, например:
+ *   ```c
+ *       tail = SLList_new_tail(tail, 3.14);
+ *   ```
+ *   Его можно будет получить при помощи цикла, но делать это
+ *   несколько раз, например в цикле, будет затратно по времени.
  */
 struct SLList_node *SLList_new_tail(
     struct SLList_node *old_tail,
     float data
 ) {
+    // Создание узла
     struct SLList_node *result = malloc(sizeof *result);
     result->data = data;
     result->next = NULL;
+
     if(old_tail != NULL)
         old_tail->next = result;
+    // Иначе список был пуст.
+
     return result;
 }
 
 
+/**
+ * @brief Очистить всю память, занимаемую списком.
+ * @param head голова (первый элемент) списка.
+ *             Если `NULL`, список пуст; действий не производится.
+ * @details Итеративная (не рекурсивная) реализация
+ */
 void SLList_delete(struct SLList_node *head) {
-    if(!head) return;
+    if(!head)  // пустой список
+        return;
 
-    struct SLList_node *cur, *next;
+    /// Итератор цикла
+    struct SLList_node *cur;
 
-    // for(cur = head; cur != NULL; cur = next)  // не подходит.
-    
-    cur = head;
-    while(cur != NULL) {
+    /// Сохраненное значение `cur->next`.
+    ///
+    /// После `free(cur)` `cur->next` недоступен напрямую
+    /// (ошибка доступа к освобожденной памяти).
+    struct SLList_node *next;
+
+    for(cur = head; cur != NULL; cur = next) {
         next = cur->next;
         free(cur);
-
-        cur = next;
     }
 }
