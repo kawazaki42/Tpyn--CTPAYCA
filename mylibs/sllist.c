c0b09cfe0b68fea9b72b86f01360d39dc8cfe13a
anonymous <anonymous>
2025-07-02 19:23:20 +0900

    (12.1) Document SLList_new_head

--- mylibs/sllist.c
+++ mylibs/sllist.c
@@ -1,15 +1,50 @@
 #include <stdlib.h>  // malloc, free, NULL
 #include "sllist.h"
 
-struct SLList_node *SLList_new_head(struct SLList_node *old_head, float data) {
+
+/**
+ * @brief Добавить элемент в начало списка (перед головой).
+ * @param old_head текущая голова списка
+ * @param data вещественное число (`float`) для добавления
+ * @return Новую голову списка
+ * 
+ * Следует сохранить новую голову списка, например:
+ * 
+ * ```c
+ *     head = SLList_new_head(head, 3.14);
+ * ```
+ * 
+ *     Иначе она будет потеряна.
+ */
+struct SLList_node *SLList_new_head(
+    struct SLList_node *old_head,
+    float data
+) {
     struct SLList_node *result = malloc(sizeof *result);
     result->data = data;
-    result->next = old_head;
+    result->next = old_head;  // Возможно, NULL
+
     return result;
 }
 
 
-struct SLList_node *SLList_new_tail(struct SLList_node *old_tail, float data) {
+/**
+ * @brief Добавить элемент в конец списка (после хвоста).
+ *   ```c
+ *       tail = SLList_new_tail(tail, 3.14);
+ *  ```
+ * @param old_tail текущий хвост списка
+ * @param data вещественное число (`float`) для добавления
+ * @return Новый хвост списка
+ * @note
+ *     Следует сохранить новый хвост списка, например:
+ * 
+ *     Иначе придется искать 
+ */
+struct SLList_node *SLList_new_tail(
+    struct SLList_node *old_tail,
+    float data
+) {
     struct SLList_node *result = malloc(sizeof *result);
     result->data = data;
     result->next = NULL;
