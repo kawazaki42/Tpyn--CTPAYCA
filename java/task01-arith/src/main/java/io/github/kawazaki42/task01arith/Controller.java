6c6c32eb96d6d7436a5f2ba3023b1aa1bc0977e2
haxxxor <212337579+kawazaki42@users.noreply.github.com>
2025-07-04 23:00:06 +0300

    (java/1) WIP. I got IDEA broken on my machine

--- java/task01-arith/src/main/java/io/github/kawazaki42/task01arith/Controller.java
+++ java/task01-arith/src/main/java/io/github/kawazaki42/task01arith/Controller.java
@@ -2,13 +2,33 @@ package io.github.kawazaki42.task01arith;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import io.github.kawazaki42.task01arith.Model;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+//    @FXML
+//    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label labelAnswerA;
+
+    @FXML
+    private Label labelAnswerB;
+
+    private TextField textInputX;
+    private TextField textInputY;
+    private TextField textInputZ;
+
+    @FXML
+    protected void onCalcButtonClick() {
+        double x, y, z;
+        x = textInputX.getText();
+        y = textInputY.getText();
+        z = textInputZ.getText();
+        Double()
+//        welcomeText.setText("Welcome to JavaFX Application!");
+        labelAnswerA.setText("a = " + Model.calcA(x, y));
+        labelAnswerB.setText("b = " + Model.calcB(z));
     }
 }
