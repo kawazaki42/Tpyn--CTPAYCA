2e1d9151f46f6a27da1429288e4ca906374c5479
student <212337579+kawazaki42@users.noreply.github.com>
2025-07-05 13:52:11 +0900

    (java/2 - loops) Skeleton

---
+++ java/task02-loops/src/main/java/io/github/kawazaki42/task02loops/Launcher.java
@@ -0,0 +1,9 @@
+package io.github.kawazaki42.task02loops;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ java/task02-loops/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.github.kawazaki42.task02loops {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens io.github.kawazaki42.task02loops to javafx.fxml;
+    exports io.github.kawazaki42.task02loops;
+}
