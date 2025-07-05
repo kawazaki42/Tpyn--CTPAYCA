676809aa4a9c667b43501f264ba89d511af8d380
student <212337579+kawazaki42@users.noreply.github.com>
2025-07-05 14:13:59 +0900

    (java/2) Added Model.java

---
+++ java/task02-loops/src/main/java/io/github/kawazaki42/task02loops/Model.java
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Логика программы (Model по MVC).
+ * @author Николай Ковалев
+ * @details
+ *     (84) Даны натуральное n, действительное х. Вычислить:
+ *       А) sin(х) + sin(х)^2 + ... + sin(х)^n;
+ */
+
+package io.github.kawazaki42.task02loops;
+
+
+/// Логика программы
+public class Model {
+    public static double calc(int n, double x) {
+        // sin(x) не меняется, заранее вычислим его
+        double sinX = Math.sin(x);
+
+        // sin(x)^i, где 1 <= i <= n
+        // Чтобы не вычислять Math.pow(sinX, i)
+        double sinXpowN = sinX;
+        double sum = 0.0d;
+        for(int i = 1; i <= n; i++) {
+            sum += sinXpowN;
+            sinXpowN *= sinX;
+        }
+
+        return sum;
+    }
+}
